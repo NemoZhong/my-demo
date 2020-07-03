cf126a3ac27faa3e38cb73cbd206fe02d16d7358
Nemo <44050946+NemoZhong@users.noreply.github.com>
2020-07-03 10:15:54 +0800

    全局loading

---
+++ src/components/globalLoading/globalLoading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mask">
+      <div class="loading">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GlobalLoading"
+};
+</script>
+
+<style lang="less" scoped>
+    .mask{
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        background-color: rgba(0, 0, 0, 0.2);
+    }
+</style>
